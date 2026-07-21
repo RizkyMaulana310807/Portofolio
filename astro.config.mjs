7528f12eb0a6e55900db1621795a52bf8906a376
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2026-07-21 18:18:13 +0700

    ci: add github actions workflow for astro deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,11 @@ import icon from 'astro-icon'; // 1. Tambahkan import ini
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  integrations: [
-    icon()
-  ],
+    site: 'https://RizkyMaulana310807.github.io',
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    integrations: [
+        icon()
+    ],
 });
